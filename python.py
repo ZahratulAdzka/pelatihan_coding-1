96a87eb3aad075ac868b2efcd061d4ea72c3cae1
ZahratulAdzka <143859489+ZahratulAdzka@users.noreply.github.com>
2023-09-08 09:55:22 +0700

    Add files via upload

---
+++ python.py
@@ -0,0 +1,40 @@
+# apel = 52,2
+# print(apel)
+# print(type(apel))
+# apel2= 52
+# print(type(apel2))
+# nama_apel = "malang"
+# print(type(nama_apel)) 
+
+# apel_benar= True
+# apel_salah= False
+# print(type(apel_benar))
+# print(apel_salah)
+
+# list_warna_apel = ['hijau','merah']
+# list_apel1 = ['manis',2,3.0]
+# print(list_warna_apel)
+# print(list_apel1)
+# print(list_apel1[2])
+
+# tuple_jumlah = (2,5,25,"satu")
+# print(tuple_jumlah)
+# print (tuple_jumlah{1})
+
+# apel__dict__ = {'nama':'apel','asal':'malang','jumlah':'54'}
+# print(apel__dict__['nama'])
+# print(apel__dict__['asal'])
+# print(apel__dict__['jumlah'])
+
+print(34+2)
+print(39-4)
+print(2*6)
+print(9/3)
+
+
+
+
+
+
+
+
